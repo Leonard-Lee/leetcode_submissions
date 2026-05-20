202a3d8b20572e954f021341b577450d36d50bb5
Leonard Lee <archerleonard@gmail.com>
2026-05-19 23:05:45 -0700

    Time: 6 ms (6.44%) | Memory: 19.7 MB (5.00%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.py
+++ 692-top-k-frequent-words/top-k-frequent-words.py
@@ -1,3 +1,8 @@
+class TrieNode:
+    def __init__(self):
+        self.children = [None] * 26
+        self.word = None
+
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
         # map a word to its frequency
@@ -5,20 +10,39 @@ class Solution:
         for word in words:
             countMap[word] += 1
 
+        def insertTrieNode(root: TrieNode, word: str) -> None:
+            cur = root
+            for i in range(len(word)):
+                ch = word[i]
+                idx = ord(ch) - ord("a")
+                if not cur.children[idx]:
+                    cur.children[idx] = TrieNode()
+
+                cur = cur.children[idx]
+            cur.word = word
+
         # it's possible that whole list has the same words
         buckets = [[] for i in range(len(words) + 1)]
         for word, count in countMap.items():
-            buckets[count].append(word)
+            if not buckets[count]:
+                buckets[count] = TrieNode()
+            insertTrieNode(buckets[count], word)
 
         res = []
+        def dfs(node: TrieNode):
+            if node.word:
+                res.append(node.word)
+
+            if len(res) == k:
+                return
+
+            for child in node.children:
+                if child:
+                    dfs(child)
+
         for i in range(len(buckets) - 1, 0, -1):
             if buckets[i]:
-                buckets[i].sort()
-            for word in buckets[i]:
-                res.append(word)
-
-                if len(res) == k:
-                    return res
-
+                dfs(buckets[i])
         return res
+
         
