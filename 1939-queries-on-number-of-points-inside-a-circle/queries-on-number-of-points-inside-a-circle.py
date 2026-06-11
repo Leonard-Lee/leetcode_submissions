1dd9bdb63fda1b4c24b3a996ef5aa88c68ed3709
Leonard Lee <archerleonard@gmail.com>
2026-06-10 20:45:35 -0700

    Time: 328 ms (98.06%) | Memory: 19.5 MB (51.08%) - LeetSync

---
+++ 1939-queries-on-number-of-points-inside-a-circle/queries-on-number-of-points-inside-a-circle.py
@@ -0,0 +1,33 @@
+class Solution:
+    def countPoints(self, points: List[List[int]], queries: List[List[int]]) -> List[int]:
+        # sort the points 
+        points.sort()
+        res = [0] * len(queries)
+
+        # use bounding box
+        for i in range(len(queries)):
+            cx, cy, r = queries[i]
+            left = cx - r
+            right = cx + r
+
+            # print("left: " + str(left) + " right: " + str(right))
+            leftIdx = self.binarySearch(points, left)
+            rightIdx = self.binarySearch(points, right + 1)
+
+            for j in range(leftIdx, rightIdx):
+                px, py = points[j]
+                if (px - cx) * (px - cx) + (py - cy) * (py - cy) <= r * r:
+                    res[i] += 1
+        return res
+
+    def binarySearch(self, points: List[List[int]], x: int) -> int:
+        l, r = 0, len(points)
+        while l < r:
+            mid = (l + r) // 2
+            if points[mid][0] >= x:
+                r = mid
+            else:
+                l = mid + 1
+        return l
+
+        
