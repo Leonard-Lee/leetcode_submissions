01e8d5895ba4fa29fb27191faee5c0e34ee37228
Leonard Lee <archerleonard@gmail.com>
2026-06-15 15:47:21 -0700

    Time: 267 ms (60.45%) | Memory: 46.4 MB (21.39%) - LeetSync

--- 1733-maximum-number-of-visible-points/maximum-number-of-visible-points.py
+++ 1733-maximum-number-of-visible-points/maximum-number-of-visible-points.py
@@ -1,40 +1,36 @@
 class Solution:
     def visiblePoints(self, points: List[List[int]], angle: int, location: List[int]) -> int:
+        if not points:
+            return 0
+
         overlap, degrees = self.transformToDegrees(points, location)
-        count = 0
-        print(degrees)
         i = 0
+        count = 0
         for j in range(len(degrees)):
             while degrees[i] + angle < degrees[j]:
                 i += 1
-            
             count = max(count, j - i + 1)
 
         return count + overlap
-        
 
-
-    # transform all the points based on the location to degrees
-    def transformToDegrees(self, points: List[List[int]], location: List[int]) -> Tuple:
+    def transformToDegrees(self, points: List[List[int]], location: List[int]) -> List[int]:
         degrees = []
         overlap = 0
+
         for px, py in points:
-            dy = py - location[1]
             dx = px - location[0]
-            if dy == 0 and dx == 0:
+            dy = py - location[1]
+
+            if dx == 0 and dy == 0:
                 overlap += 1
-                continue 
+                continue
 
             degree = math.degrees(math.atan2(dy, dx))
             if degree < 0:
                 degree += 360
+
             degrees.append(degree)
 
         degrees.sort()
         return (overlap, degrees + [d + 360 for d in degrees])
-
-
-
-
-
         
