aba11f81e7205ee6e26318b4bac0ecfe18fe52eb
Leonard Lee <archerleonard@gmail.com>
2026-06-15 15:23:17 -0700

    Time: 633 ms (5.22%) | Memory: 54 MB (6.96%) - LeetSync

--- 1733-maximum-number-of-visible-points/maximum-number-of-visible-points.py
+++ 1733-maximum-number-of-visible-points/maximum-number-of-visible-points.py
@@ -1,33 +1,40 @@
 class Solution:
     def visiblePoints(self, points: List[List[int]], angle: int, location: List[int]) -> int:
-        overlap_count, degrees = self.pointsToDegrees(points, location)
+        overlap, degrees = self.transformToDegrees(points, location)
+        count = 0
+        print(degrees)
         i = 0
-        maxCount = 0
         for j in range(len(degrees)):
-            while degrees[j] > degrees[i] + angle:
+            while degrees[i] + angle < degrees[j]:
                 i += 1
-            maxCount = max(maxCount, j - i + 1)
-        return maxCount + overlap_count
+            
+            count = max(count, j - i + 1)
 
-    def pointsToDegrees(self, points: List[List[int]], location: List[int]) -> (int, List[List[int]]):
-        n = len(points)
-        angles = []
-        overlap_count = 0
-        for x, y in points:
-            dx = x - location[0]
-            dy = y - location[1]
-            if dx == 0 and dy == 0:
-                overlap_count += 1
-            else:
-                degree = math.degrees(math.atan2(dy, dx))
+        return count + overlap
+        
 
-                if degree < 0:
-                    degree += 360
 
-                angles.append(degree)
+    # transform all the points based on the location to degrees
+    def transformToDegrees(self, points: List[List[int]], location: List[int]) -> Tuple:
+        degrees = []
+        overlap = 0
+        for px, py in points:
+            dy = py - location[1]
+            dx = px - location[0]
+            if dy == 0 and dx == 0:
+                overlap += 1
+                continue 
 
-        angles.sort()
-        length = len(angles)
-        for i in range(length):
-            angles.append(angles[i] + 360)
-        return (overlap_count, angles)
+            degree = math.degrees(math.atan2(dy, dx))
+            if degree < 0:
+                degree += 360
+            degrees.append(degree)
+
+        degrees.sort()
+        return (overlap, degrees + [d + 360 for d in degrees])
+
+
+
+
+
+        
