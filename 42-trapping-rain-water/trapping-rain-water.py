7570ce48923864a11cd12c7b1745d19445ba213c
Leonard Lee <archerleonard@gmail.com>
2026-06-17 09:47:37 -0700

    Time: 3 ms (95.12%) | Memory: 21 MB (48.74%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -3,22 +3,23 @@ class Solution:
         if not heights:
             return 0
 
+        l, r = 0, len(heights) - 1
         lmax, rmax = 0, 0
-        n = len(heights)
-        # two pointers
-        l, r = 0, n - 1
         count = 0
+
         while l < r:
             if heights[l] < heights[r]:
-                if heights[l] > lmax:
-                    lmax = heights[l]
-                else:
+                if heights[l] < lmax:
                     count += lmax - heights[l]
+                else:
+                    lmax = heights[l]
                 l += 1
             else:
-                if heights[r] > rmax:
-                    rmax = heights[r]
-                else:
+                if heights[r] < rmax:
                     count += rmax - heights[r]
+                else:
+                    rmax = heights[r]
                 r -= 1
-        return count 
+
+        return count
+        
