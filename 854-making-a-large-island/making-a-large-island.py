7b1d1e70b60eb193270548e589499fe63b8f95aa
Leonard Lee <archerleonard@gmail.com>
2026-07-10 09:27:44 -0700

    Time: 1339 ms (20.34%) | Memory: 36.4 MB (63.81%) - LeetSync

--- 854-making-a-large-island/making-a-large-island.py
+++ 854-making-a-large-island/making-a-large-island.py
@@ -1,62 +1,52 @@
 class Solution:
+    # visit each island, tag them, and calculate their area
+    # map tag nums to area
+    # iterate all 0 cells, find the max area
     def largestIsland(self, grid: List[List[int]]) -> int:
-        # tage each island 
-        # hash map mapping tag number to area 
-        # go through all the 0s and flip them to connect the largest area
-
         if not grid or not grid[0]:
             return 0
 
         rows = len(grid)
         cols = len(grid[0])
-        visitSet = set()
 
-        def dfs(r, c, tag) -> int:
+        # return total area
+        def dfs(r: int, c: int, tag: int) -> int:
             if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] != 1:
                 return 0
 
             grid[r][c] = tag
             area = 1
-            area += dfs(r + 1, c, tag)
-            area += dfs(r - 1, c, tag)
-            area += dfs(r, c + 1, tag)
-            area += dfs(r, c - 1, tag)
+            area += dfs(r + 1, c, tag) + dfs(r - 1, c, tag) + dfs(r, c + 1, tag) + dfs(r, c - 1, tag)
+        
             return area
 
         mapping = {}
         tag = 2
-        maxArea = 0
+
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == 1:
                     area = dfs(r, c, tag)
+                    print(area)
                     mapping[tag] = area
-                    maxArea = max(maxArea, area)
                     tag += 1
 
-        def helper(r, c):
-            directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-            area = 1
-            visitTags = [0]
-
-            for dr, dc in directions:
-                newr = r + dr
-                newc = c + dc
-                if newr in range(rows) and newc in range(cols):
-                    tag = grid[newr][newc]
-                    if tag not in visitTags:
-                        area += mapping[tag]
-                        visitTags.append(tag)
-            return area
-
+        dirs = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        maxArea = 0
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == 0:
-                    area = helper(r, c)
-                    maxArea = max(maxArea, area)
-        
-        return maxArea
+                    visitSet = set()
+                    area = 1
+                    for dr, dc in dirs:
+                        newr = r + dr
+                        newc = c + dc
+                        if newr in range(rows) and newc in range(cols) and grid[newr][newc] not in visitSet and grid[newr][newc] != 0:
+                            tag = grid[newr][newc]
+                            area += mapping[tag]
+                            visitSet.add(tag)
+                        maxArea = max(maxArea, area)
+
+        return maxArea if maxArea != 0 else rows * cols
 
 
-            
-        
