32735aa8c8e2a66106382dca4099190572d31e1d
Leonard Lee <archerleonard@gmail.com>
2026-07-14 13:11:16 -0700

    Time: 198 ms (45.03%) | Memory: 66.4 MB (30.03%) - LeetSync

--- 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.py
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.py
@@ -1,22 +1,31 @@
 class Solution:
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
         intervals = sorted(zip(startTime, endTime, profit))
-
-        n = len(startTime)
         dp = {}
-        dp[n] = 0
-        # regarding the idx, return the max profit
+
+        # input: cur idx
+        # output: max profit
         def dfs(idx: int) -> int:
             if idx in dp:
                 return dp[idx]
 
-            # skip the current profit
-            res = dfs(idx + 1)
-            # take the current profit and find the next start index
-            newIdx = bisect.bisect_left(intervals, intervals[idx][1], key=lambda x: x[0])
-            dp[idx] = max(res, intervals[idx][2] + dfs(newIdx))
+            if idx >= len(intervals):
+                dp[idx] = 0
+                return 0
+
+            # not pick
+            skip = dfs(idx + 1)
+
+            # pick the current interval
+            newIdx = bisect.bisect_left(intervals, (intervals[idx][1], -1, -1))
+            include = intervals[idx][2] + dfs(newIdx)
+
+            dp[idx] = max(skip, include)
             return dp[idx]
 
         return dfs(0)
+
+
+
 
         
