b1be54da749aba636f132c37b69f1a2cdaa37627
Leonard Lee <archerleonard@gmail.com>
2026-07-28 12:04:18 -0700

    Time: 247 ms (89.01%) | Memory: 34.1 MB (30.30%) - LeetSync

--- 4041-climbing-stairs-ii/climbing-stairs-ii.py
+++ 4041-climbing-stairs-ii/climbing-stairs-ii.py
@@ -3,16 +3,26 @@ class Solution:
         newCosts = [0] + costs
 
         # n is the stair and the output is the minimum cost
-        memo = {}
-        def dfs(n) -> int:
-            if n in memo:
-                return memo[n]
-            if n <= 0:
-                memo[n] = 0
-                return 0
+        # memo = {}
+        # def dfs(n) -> int:
+        #     if n in memo:
+        #         return memo[n]
+        #     if n <= 0:
+        #         memo[n] = 0
+        #         return 0
 
-            memo[n] = newCosts[n] + min(9 + dfs(n - 3), 4 + dfs(n - 2), 1 + dfs(n - 1))
-            return memo[n]
+        #     memo[n] = newCosts[n] + min(9 + dfs(n - 3), 4 + dfs(n - 2), 1 + dfs(n - 1))
+        #     return memo[n]
 
-        return dfs(n)
+        # return dfs(n)
+        memo = [0] * (n + 1)
+        memo[0] = 0
+        for i in range(1, n + 1):
+            jump1 = 1 + memo[i - 1] if i >= 1 else float("inf")
+            jump2 = 4 + memo[i - 2] if i >= 2 else float("inf")
+            jump3 = 9 + memo[i - 3] if i >= 3 else float("inf")
+            memo[i] = newCosts[i] + min(jump1, jump2, jump3)
+
+        return memo[n]
+
         
