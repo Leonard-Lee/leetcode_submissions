2db1dab2c8445d3220908e6d0e6d9e9637eee000
Leonard Lee <archerleonard@gmail.com>
2026-07-30 22:47:09 -0700

    Time: 98 ms (71.50%) | Memory: 22.9 MB (96.15%) - LeetSync

--- 2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.py
+++ 2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.py
@@ -2,24 +2,42 @@ class Solution:
     def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
         MOD = 10 ** 9 + 7
 
-        # "00", "11", "000", "110", "011"
-        memo = {}
-        def dfs(idx) -> int:
-            if idx in memo:
-                return memo[idx]
+        memo = [0] * (high + 1)
+        memo[0] = 1
 
-            if idx > high:
-                memo[idx] = 0
-                return 0
+        for i in range(high + 1):
+            if i >= zero:
+                memo[i] += memo[i - zero] 
+            if i >= one:
+                memo[i] += memo[i - one]
 
-            count = 0
-            if idx >= low:
-                count = 1
+            memo[i] %= MOD
 
-            memo[idx] = (count + dfs(idx + zero) + dfs(idx + one)) % MOD
-            return memo[idx]
+        total = 0
+        for i in range(low, high + 1):
+            total += memo[i]
         
-        return dfs(0)
+        total %= MOD
+        return total
+
+        # "00", "11", "000", "110", "011"
+        # memo = {}
+        # def dfs(idx) -> int:
+        #     if idx in memo:
+        #         return memo[idx]
+
+        #     if idx > high:
+        #         memo[idx] = 0
+        #         return 0
+
+        #     count = 0
+        #     if idx >= low:
+        #         count = 1
+
+        #     memo[idx] = (count + dfs(idx + zero) + dfs(idx + one)) % MOD
+        #     return memo[idx]
+        
+        # return dfs(0)
 
 
         
